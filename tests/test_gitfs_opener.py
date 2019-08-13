87954717e6a41c43695315c766d3d157db68085a
chfw <wangc_2011@hotmail.com>
2019-08-13 23:13:16 +0100

    :green_heart: python fs2 hates str on python 2

--- tests/test_gitfs_opener.py
+++ tests/test_gitfs_opener.py
@@ -9,7 +9,7 @@ def test_opener(fake_clone):
     fake_clone.return_value = '.'
     url = "git://github.com/moremoban/pypi-mobans.git?submodule=true!/tests"
     file_system = fs.open_fs(url)
-    ok_(file_system.exists('test_gitfs_opener.py'))
+    ok_(file_system.exists(u'test_gitfs_opener.py'))
     fake_clone.assert_called_with(
         GitRequire(
             git_url='git://github.com/moremoban/pypi-mobans.git',
