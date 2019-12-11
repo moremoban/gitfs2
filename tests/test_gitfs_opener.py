5376889fc0056901de022b71916846343d1c09fa
chfw <wangc_2011@hotmail.com>
2019-12-11 09:28:20 +0000

    :green_heart: fix mock dependency issue. python 3 has it in unittest so we are not installing it in python 3

--- tests/test_gitfs_opener.py
+++ tests/test_gitfs_opener.py
@@ -1,5 +1,9 @@
 import fs
-from mock import patch
+try:
+    from mock import patch
+except ImportError:
+    from unittest.mock import patch
+
 from nose.tools import ok_
 from gitfs2.repo import GitRequire
 
