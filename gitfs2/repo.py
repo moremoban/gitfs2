b70d4273df6bcceed14ca41aa4a0e7b685b53e30
chfw <wangc_2011@hotmail.com>
2024-10-18 15:55:34 +0100

    :update: fix github github.com (port 9418)  timeout problem

--- gitfs2/repo.py
+++ gitfs2/repo.py
@@ -16,7 +16,7 @@ class GitRequire(object):
     def __init__(
         self, git_url=None, branch=None, submodule="False", reference=None
     ):
-        self.git_url = git_url
+        self.git_url = git_url.replace("git://", "ssh://git@")
         self.submodule = convert_submodule(submodule)
         self.branch = branch
         self.reference = reference
